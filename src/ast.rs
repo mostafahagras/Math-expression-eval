0ec2c384f9e8455911dda0513df3ae4d9d6aee83
mostafahagras <mostafahagras07@gmail.com>
2024-10-11 14:36:47 +0300

    Remove AstNode::Group

--- src/ast.rs
+++ src/ast.rs
@@ -3,7 +3,6 @@ pub enum AstNode {
     Number(f64),
     BinaryOp(Box<AstNode>, Operator, Box<AstNode>),
     UnaryOp(UnaryOp, Box<AstNode>),
-    Group(Box<AstNode>),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -45,10 +44,52 @@ impl Operator {
 impl AstNode {
     pub fn eval(&self) -> f64 {
         match self {
-            AstNode::Group(grp) => grp.eval(),
             AstNode::BinaryOp(lhs, op, rhs) => op.eval(lhs, rhs),
             AstNode::Number(number) => *number,
             AstNode::UnaryOp(unary_op, number) => unary_op.eval(number),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parser::Parser, tokenizer::Tokenizer};
+
+    #[test]
+    fn test() {
+        let expressions = vec![
+            "1 + 1",
+            "2 * 3",
+            "4 - 2",
+            "6 / 3",
+            "2 + 3 * 4",
+            "10 / 2 - 1",
+            "2(3 + 4)",
+            "(2 + 3) * 4",
+            "(1 - 2) * (3 + 4)",
+            "2(2)(3)",
+            "(2 + 3)(4 - 1)",
+            "2 + (3 * 4) - (1 / 2)",
+            "3 * (2 + 1)",
+            "(5 - 2) * (1 + 2) / 3",
+            "2 + (4 * 2) * 2",
+            "(2)(2)",
+            "2(2 + 3)",
+            "(2 - 1)(3 + 2)",
+            "-3 + 5",
+            "1 + 2 * 3 - 4 / 5",
+        ];
+        let expected_results = vec![
+            2.0, 6.0, 2.0, 2.0, 14.0, 4.0, 14.0, 20.0, -7.0, 12.0, 15.0, 13.5, 9.0, 3.0, 18.0, 4.0,
+            10.0, 5.0, 2.0, 6.2,
+        ];
+        for i in 0..20 {
+            let mut tokenizer = Tokenizer::new(expressions[i]);
+            let tokens = tokenizer.tokens();
+            let mut parser = Parser::new(tokens);
+            let ast = parser.parse();
+            let result = ast.eval();
+            assert_eq!(result, expected_results[i]);
         }
     }
 }
